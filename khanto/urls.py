cc168ac01f8f6d36f365dbf8352ca5b91342f107
Matheus Pinheiro <matheus.passos@jointecnologia.com.br>
2023-06-30 15:22:09 -0300

    Registered django rest routers to properties, announcements and bookings.

--- khanto/urls.py
+++ khanto/urls.py
@@ -1,9 +1,13 @@
 from django.urls import include, path
 from rest_framework import routers
 
-router = routers.SimpleRouter()
+from khanto.views import PropertyViewSet, AnnouncementViewSet, BookingViewSet
 
+router = routers.DefaultRouter()
+router.register(r'properties', PropertyViewSet)
+router.register(r'announcements', AnnouncementViewSet)
+router.register(r'bookings', BookingViewSet)
 
 urlpatterns = [
-    path('home/', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
