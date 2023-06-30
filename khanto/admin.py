59f8b398ff8b21a42e3c3a5e2cc9ba32b74498aa
Matheus Pinheiro <matheus.passos@jointecnologia.com.br>
2023-06-30 15:19:31 -0300

    Refactoring admin and added BookinAdmin class to handle read only fields and list display.

--- khanto/admin.py
+++ khanto/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from khanto.models import *
 
+from khanto.models import Announcement, Booking, Property
+
+
+class BookingAdmin(admin.ModelAdmin):
+    """This class was created to set some specifics behaviors to booking admin."""
+    readonly_fields = ('total_price',)
+    list_display = ('code', 'announcement', 'check_in',
+                    'check_out', 'number_of_guests', 'total_price')
+
+
+admin.site.register(Booking, BookingAdmin)
 admin.site.register(Property)
 admin.site.register(Announcement)
-admin.site.register(Booking)
