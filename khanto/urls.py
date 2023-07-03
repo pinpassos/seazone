102ba60fd4630ed39cb02146b5776298a5a2f862
Matheus Pinheiro <matheus.passos@jointecnologia.com.br>
2023-07-03 19:46:41 -0300

    Added basename to routers.

--- khanto/urls.py
+++ khanto/urls.py
@@ -4,9 +4,9 @@ from rest_framework import routers
 from khanto.views import PropertyViewSet, AnnouncementViewSet, BookingViewSet
 
 router = routers.DefaultRouter()
-router.register(r'properties', PropertyViewSet)
-router.register(r'announcements', AnnouncementViewSet)
-router.register(r'bookings', BookingViewSet)
+router.register(r'properties', PropertyViewSet, basename='properties-api')
+router.register(r'announcements', AnnouncementViewSet, basename='announcements-api')
+router.register(r'bookings', BookingViewSet, basename='bookings-api')
 
 urlpatterns = [
     path('api/', include(router.urls)),
